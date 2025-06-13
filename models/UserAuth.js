7b930e96689d145584313bbec2aac2d07800fce2
Hanish SS <hanishgowda7795@gmail.com>
2025-06-13 23:01:59 +0530

    Login backend updated 5

--- models/UserAuth.js
+++ models/UserAuth.js
@@ -1,21 +1,10 @@
 import mongoose from 'mongoose';
 
 const registerSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
+  username: String,
+  phone: { type: String, unique: true },
+  password: String,
 });
 
 const Register = mongoose.model('Register', registerSchema);
-
 export default Register;
